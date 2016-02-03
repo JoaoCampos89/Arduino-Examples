6a8e75e467ed91a7ec7063892c6aa04227b4aa4f
JoaoCampos89 <jcampos893@gmail.com>
2016-02-03 21:20:12 -0200

    fix problem

--- muxExample/muxExample.ino
+++ muxExample/muxExample.ino
@@ -16,6 +16,9 @@ const int portY0 = 12;
 float sensor[4];
 
 int count = 0;   //which y0 pin we are selecting
+ // declare a timer to be used by mux task
+unsigned long muxTimer = 0; 
+
  
 void setup() {
  // digital pin X0 defined as output
@@ -28,6 +31,9 @@ void setup() {
 }
 
 void loop() {
+
+
+	
 // fill array sensor with mux analog ports switching each 10 ms
   muxTask(sensor,10);
 
@@ -49,18 +55,23 @@ void loop() {
  */
  
 void muxTask(float reading[], int timeInterval){
-   
- // digital pins that selects the sensor
-  int x0 = 0;      
-  int x1 = 0;     
+  // disable update of muxTimer
+   flagMux = false;
+	// digital pins that selects the sensor
+	int x0 = 0;      
+	int x1 = 0;     
   // getting the actual time
-   unsigned long mili = millis();
-
-   int atualInterval =  mili - timeInterval;
+   unsigned long currentMillis = millis();
+   
+   int atualInterval = currentMillis - muxTimer;
    // if time is bigger than the defined interval change the mux digital pins and performs the reading in the 
    // common port
    if(atualInterval > timeInterval)
    {
+		// updates mux timer
+		muxTimer = currentMillis;
+		// enable flag to update muxTimer
+		flagMux = true;
         count++;
         
         if(count >3)
